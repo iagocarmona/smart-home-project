82ea5315fbb3893fd0ceec31b9432074799f5a12
Iago Carmona <iagoortegacarmona@gmail.com>
2023-11-12 20:04:16 -0300

    creating app mobile

---
+++ mobile/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { NativeBaseProvider, StatusBar } from 'native-base'
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { THEME } from './src/styles/theme'
+
+import { Loading } from './src/components/Loading'
+
+import { Routes } from './src/routes'
+
+export default function App() {
+  const [fontLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular })
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontLoaded ? <Routes /> : <Loading />}
+    </NativeBaseProvider>
+  )
+}
